888eeed55d331334bb73425c70ba6b2e982663cd
Vinay Joshi <joshivinay085@gmail.com>
2024-01-13 08:47:16 +0530

    Swapping numbers

---
+++ swap2.js
@@ -0,0 +1,10 @@
+
+exports.swap = function(a,b){
+
+    var a;
+    var b;
+    a = a + b;   //a = 15
+    b = a - b;   //b = 5
+    a = a - b;
+    console.log(a,b);
+}
---
+++ swapped.js
@@ -0,0 +1,2 @@
+const check = require('./swap2');
+console.log(check.swap(4,3));
